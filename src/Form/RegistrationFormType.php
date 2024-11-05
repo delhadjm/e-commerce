20fd645b023123af333375c9d67b24eb4b25a600
delhadjm <cirelaria@gmail.com>
2024-11-05 19:57:57 +0100

    register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,7 +20,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'attr' => ['autocomplete' => 'email'],
+                'attr' => ["class" => "mr-0 md-mr-2", "placeholder" => "Email"],
+            ])
+            ->add('pseudo', TextType::class, [
+                'attr' => ['autocomplete' => 'pseudo'],
+                'attr' => ["class" => "", "placeholder" => "Pseudo"],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -26,21 +36,24 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'first_options'  => ['label' => 'Password', 'attr' => ["class" => "mr-0 md-mr-2", "placeholder" => "Password"]],
+                'second_options' => ['label' => 'Repeat Password', 'attr' => ["class" => "", "placeholder" => "Repeat Password"]],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
                     ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                    #new Length([
+                     #   'min' => 6,
+                     #   'minMessage' => 'Your password should be at least {{ limit }} characters',
+                      #  // max length allowed by Symfony for security reasons
+                      #  'max' => 4096,
+                    #]),
                 ],
             ])
         ;
